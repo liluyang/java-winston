b7f13e7a5697ffd760137e622e7cee3f43be251a
Luyang Li <liluyang@gmail.com>
2015-02-10 20:50:17 -0800

    .

--- src/problem/winston/Interest.java
+++ src/problem/winston/Interest.java
@@ -1,14 +1,34 @@
 package problem.winston;
 
 public class Interest {
+	/**
+	 * If John inherits $100,000 from family and invest all in stock market, and every year he gets
+	 * 5% interest. He puts all his earned interest into stock market again, year after year.
+	 * 
+	 * When he reaches 65-year old, how much money will John have?
+	 */
 	public double johnAsset() {
 		return 0;
 	}
 	
+	/**
+	 * If Susan saves $10,000 from her salary every year and invest in stock market, it's the same,
+	 * every year she gets 5% interest and invest all into stock market again.
+	 *
+	 * When she reaches 65-year old, how much money will Susan have?
+	 */
 	public double susanAsset() {
 		return 0;
 	}
 	
+	/**
+	 * All condition as the same in Problem 2. Now Susan is 65-year old, she retires happily.
+	 * Every year, she will withdraw $50,000 and spend as living cost. Rest of the money are still
+	 * in stock market. How much will she have at age 95?
+	 * 
+	 * @param livingCost
+	 * @return money amount Susan has at age 95
+	 */
 	public double susan95Asset(double livingCost) {
 		return 0;
 	}
