042619725b27c3fcdafc91f051e9e63d47465c1a
Luyang Li <liluyang@google.com>
2018-12-27 19:39:25 -0800

    .

--- src/problem/maze/Maze.java
+++ src/problem/maze/Maze.java
@@ -1,15 +1,33 @@
 package problem.maze;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+
 
 public class Maze {
-  public List<String> dfs(Room start, Room end) {
-    // TODO: Return room names by order, from start to end. If cannot find such path, return null.
-    return null;
-  }
-  
-  public List<String> bfs(Room start, Room end) {
-    // TODO: Return room names by order, from start to end. If cannot find such path, return null.
-    return null;
-  }
+	public static ArrayList<String> path = new ArrayList<String>();
+	
+	public List<String> dfs(Room start, Room end) {
+		if(start == null) {
+			path.remove(path.size() - 1);
+			return null;
+		}
+		for(Room room : start.getNeighbors()) {
+			if(!path.contains(end.getName())) {
+				path.add(start.getName());
+				dfs(room, end);
+			}
+		}
+		return path;
+	}
+	
+	public List<String> bfs(Room start, Room end) {
+		Queue<Room> queue = new LinkedList<Room>();
+		while(!queue.contains(end)) {
+			queue.addAll(start.getNeighbors());
+		}
+		return null;
+	}
 }
